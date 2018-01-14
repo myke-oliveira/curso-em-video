499859cee1efdd7c8cccdbbe1a5e7687095b3fcb
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2018-01-14 14:12:55 -0300

    Mundo 2, primeiro dia.

---
+++ aula011.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3.6
+
+print('\033[0;30;41mTeste\033[m')
+print('\033[4;33;44mTeste\033[m')
+print('\033[0;35;43mTeste\033[m')
+print('\033[1;30;42mTeste\033[m')
+print('\033[mTeste')
+print('\033[7;30mTeste\033[m')
+
